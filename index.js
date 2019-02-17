d841e105ae6ef33aca648677a366227433c6b048
ry0-ta <ry0ta4dev@gmail.com>
2019-02-17 17:44:02 +0900

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const server = require("express")();
+const line = require("@line/bot-sdk");
+
+const line_config = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+
+server.listen(process.env.port || 3000);
+
